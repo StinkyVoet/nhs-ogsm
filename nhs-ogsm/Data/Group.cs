0641e311842978a5bddecfabe9cf11707b99ea7d
Rik R <rik@remijn.org>
2024-01-09 16:41:01 +0100

    base for GroupServiceTest.cs added equals override to Group.cs

--- nhs-ogsm/Data/Group.cs
+++ nhs-ogsm/Data/Group.cs
@@ -7,4 +7,14 @@ public class Group
     public int ID { get; set; }
     public string Name { get; set; }
     public ICollection<User> Users { get; set; } = new List<User>();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType()) return false;
+     
+        Strategy other = (Strategy)obj;
+        return
+            ID == other.ID &&
+            Name == other.Name;
+    }
 }
---
+++ nhs-ogsmTest/ServicesTest/GroupServiceTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nhs_ogsm.Data;
+using nhs_ogsm.Services;
+
+namespace nhs_ogsmTest;
+
+[TestClass]
+public class GroupServiceTest
+{
+    private IDbContextFactory<OgsmDbContext> _contextFactory = null!;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _contextFactory = new TestFactory();
+    }
+    
+    [TestCleanup]
+    public void CleanUp()
+    {
+        TestFactory.CleanDbContext();
+    }
+
+    [TestMethod]
+    public void AddUserToGroupTest()
+    {
+        // Arrange
+        var service = new GroupService(_contextFactory);
+        Group group1 = new Group() { Name = "s1" };
+        
+        // Act
+        
+        // Assert
+        
+    }
+}
